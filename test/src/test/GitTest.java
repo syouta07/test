394d2c4b8af49126a71687cfa343c653b6f947ef
syouta07 <a14ba018@gmail.com>
2020-12-08 14:12:13 +0900

    職業訓練 試作

--- test/src/test/GitTest.java
+++ test/src/test/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub!");
+		System.out.println("Hello GitHub!");
 	}
 
 }
